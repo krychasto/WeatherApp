03127a113ca04c753abe7226ed1ca0606bccfd68
krychasto <krystian.freus@wp.pl>
2021-04-20 17:01:09 +0200

    Initialize weather app and first api call

---
+++ weather/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+import requests
+
+def index(request):
+    response = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=50f91fe67a5661be4a20aa945d246ba3')
+    print(response.json())
+    weather_obj = response.json()
+    print(weather_obj['weather'][0]['main'])
+    return render(request, 'weather/index.html')
+
+def home(request):
+    return HttpResponseRedirect('weather/')
