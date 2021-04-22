b816eade7d8b6eada38427cf39d0b4ae41123b1c
krychasto <krystian.freus@wp.pl>
2021-04-22 17:17:14 +0200

    Basic app look

--- weather/views.py
+++ weather/views.py
@@ -6,8 +6,10 @@ def index(request):
     response = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=50f91fe67a5661be4a20aa945d246ba3')
     print(response.json())
     weather_obj = response.json()
-    print(weather_obj['weather'][0]['main'])
-    return render(request, 'weather/index.html')
+    print(weather_obj['weather'][0])
+    return render(request, 'weather/index.html', {
+        "weather": weather_obj,
+    })
 
 def home(request):
     return HttpResponseRedirect('weather/')
